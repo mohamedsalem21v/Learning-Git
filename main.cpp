b51d6bd24b63e4bfac94b627f9108f90049f5a3f
mohamed <mohamedsalem462006@gmail.com>
2025-09-04 01:02:30 +0300

    remove a function

--- main.cpp
+++ main.cpp
@@ -14,14 +14,7 @@ const int MOD = 1e9+7;
 #define int long long
 using namespace std;
 
-void FastIo()
-{
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
-    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-}
+
 
 void solve()
 {
@@ -30,7 +23,6 @@ void solve()
 
 signed main()
 {
-    FastIo();
     int test = 1;
     // cin >> test;
     for(int tc = 1; tc <= test; tc++)
