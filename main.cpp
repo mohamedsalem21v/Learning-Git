a91a82307f7a0a25683e9fa8a50c332fcffed5fa
mohamed <mohamedsalem462006@gmail.com>
2025-09-04 01:14:41 +0300

    aa

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define sp " "
+#define endl "\n"
+const int MOD = 1e9+7;
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define ve vector<int>
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+#define fixed(n) fixed << setprecision(n)
+#define input(v) for (auto &it : v) cin >> it
+#define output(v) for (auto &it : v) cout << it << " "
+#define int long long
+using namespace std;
+
+
+
+void solve()
+{
+    
+}
+
+signed main()
+{
+    int test = 1;
+    // cin >> test;
+    for(int tc = 1; tc <= test; tc++)
+    {
+        solve();
+    }
+}
