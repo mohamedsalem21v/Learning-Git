7f6e993f5213a4dc9233e70cdfd228114366f4eb
mohamed <mohamedsalem462006@gmail.com>
2025-09-03 20:00:05 +0300

    fa

--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
 #include <bits/stdc++.h>
 #define sp " "
 #define endl "\n"
+const int MOD = 1e9+7;
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define ve vector<int>
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+#define fixed(n) fixed << setprecision(n)
+#define input(v) for (auto &it : v) cin >> it
+#define output(v) for (auto &it : v) cout << it << " "
+#define int long long
 using namespace std;
-int main()
+
+void FastIo()
 {
-    cout << "hello";
-    return 0;
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+}
+
+void solve()
+{
+    
+}
+
+signed main()
+{
+    FastIo();
+    int test = 1;
+    // cin >> test;
+    for(int tc = 1; tc <= test; tc++)
+    {
+        solve();
+    }
 }
